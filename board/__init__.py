e1b3a6f73bdd82251908c3645feebee35d87ccd9
Wayne <waynechemicalengineer@gmail.com>
2024-12-18 20:54:42 -0800

    latest commit from laptop

--- board/__init__.py
+++ board/__init__.py
@@ -2,14 +2,13 @@
 # reference: https://realpython.com/flask-project/#leverage-blueprints
 from flask import Flask
 import os
-from . import posts, pages, errors
+from . import posts, pages, errors, register
 from dotenv import load_dotenv # load .env file
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 from .extensions import db
 import logging
+from datetime import timedelta
 from .dash_app import create_dash_app
-# from . import models  # Import models here to avoid circular import
-
 
 # load .env file
 load_dotenv()
@@ -20,25 +19,51 @@ def create_app():
     # start the flask instance
     app = Flask(__name__)
 
+    # Configure logging to file
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
+    # Log a message
+    app.logger.debug("Debugging information at app start...")
+
     # enabling the use of environment variables, assess to all variables with "Flask_"
     app.config.from_prefixed_env()
 
     # get Flask from Environment
     app.config["ENVIRONMENT"] = os.getenv("ENVIRONMENT")
 
+
     # get Database from Environment, must be defined before db.init_app(app)
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+
+    # Set session lifetime to 30 minutes (this dictates the duration of the user's session before logging out)
+    app.permanent_session_lifetime = timedelta(minutes=30)
 
     # initiate database
     db.init_app(app)
 
+    # app.secret_key = 'your_secret_key'
+
     # Create tables if they don't exist
     with app.app_context():
-        db.create_all()  # Create database tables if they don't exist
+        try:
+            with db.engine.begin() as connection: # begin() automatically manages the transaction, which often resolves issues like schema creation errors by ensuring everything runs within a properly handled transaction context
+                app.logger.debug('Creating schema')
+                connection.execute(text("CREATE SCHEMA IF NOT EXISTS message_board"))
+                connection.execute(text("CREATE SCHEMA IF NOT EXISTS user_info"))
+                app.logger.debug('Schema created or already exists')
+                
+            
+            # Create all tables, including those in 'user_info' schema
+            db.create_all()
+            app.logger.debug("Tables created or already exist")
+
+        except Exception as e:
+            app.logger.error(f"Error creating schema or tables: {e}")
 
     # register blueprints, must be after initiating the database and creating the tables
     app.register_blueprint(pages.bp)
     app.register_blueprint(posts.bp)
+    app.register_blueprint(register.bp)
     app.register_error_handler(404, errors.page_not_found)
 
     # setup Dash app
--- board/models.py
+++ board/models.py
@@ -11,3 +11,16 @@ class Posts(db.Model):
     author = db.Column(db.String(50), unique=True, nullable=False)
     message = db.Column(db.Text, nullable=False)
     created = db.Column(db.DateTime, default=datetime.now())  # Automatically set to current time when created
+
+
+class User(db.Model):
+    __tablename__ = 'logins'
+
+    __table_args__ = {'schema': 'user_info'}  # specify the schema
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
+    # def __repr__(self):
+    #     return f'<User {self.username}>'
